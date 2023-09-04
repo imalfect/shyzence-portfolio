ba941f3feabbc558f5f823febc4fca1cfabbcaa8
iMalFect <me@imal.dev>
2023-09-04 22:00:51 +0200

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react';
 import EventEmitter from "eventemitter3";
 import TopBar from "./components/AppBar.jsx";
 import Hero from "./components/Hero.jsx";
