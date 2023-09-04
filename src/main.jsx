11d4e1a7c639443d931981c70fc3cb26b06725e2
iMalFect <me@imal.dev>
2023-09-04 22:05:42 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import {CssBaseline, ThemeProvider} from "@mui/material";
 import themes from "./themes.js";
+// Better to make a function
 function Website() {
     return (
         <ThemeProvider theme={themes[0]}>
