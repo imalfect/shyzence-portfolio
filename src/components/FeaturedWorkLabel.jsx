623cfbed3f60480c23dd6507611ee6697e99b091
Shyzence <117179990+Shyzence@users.noreply.github.com>
2023-09-07 13:48:44 -0400

    Update FeaturedWorkLabel.jsx

--- src/components/FeaturedWorkLabel.jsx
+++ src/components/FeaturedWorkLabel.jsx
@@ -3,7 +3,7 @@ import Typography from "@mui/material/Typography";
 
 export default function FeaturedWorkLabel() {
     return (
-        <Typography variant="h4" component="h4" sx={{textAlign: 'center', mt: 2}} id="work">
+        <Typography variant="h4" component="h4" sx={{textAlign: 'left', mt: 2}} id="work">
            Featured Work
         </Typography>
     )
