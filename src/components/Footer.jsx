ce84703757e7e72d20a2cf6edcc29edf058f481e
Shyzence <117179990+Shyzence@users.noreply.github.com>
2023-09-07 16:02:11 -0400

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
     return (
         <Box sx={{flexGrow: 1, backgroundColor: '#323336', boxShadow: 50, textAlign: 'center', mt: 5, pt: 1, pb: 1}}>
             <Container>
-                 <Typography variant="p" style={{ fontSize: '10px' }}>
+                 <Typography variant="p" style={{ fontSize: '12px' }}>
     Copyright ©2023. Made with 🤍 by &nbsp;
     <a href={"https://github.com/imalfect"} style={{ textDecoration: 'none', color: '#cd3c6d' }}>
         iMalFect
