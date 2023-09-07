17dd2dd3aed1577760d2fdb69b8c580c90f6ae46
Shyzence <117179990+Shyzence@users.noreply.github.com>
2023-09-07 13:35:12 -0400

    Update Footer.jsx
    decolor

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,7 +8,7 @@ export default function Footer() {
         <Box sx={{flexGrow: 1, backgroundColor: '#323336', boxShadow: 50, textAlign: 'center', mt: 5, pt: 1, pb: 1}}>
             <Container>
                     <Typography variant="p">
-                        Copyright ©2023. Made with ❤️ by &nbsp;
+                        Copyright ©2023. Made with ♡ by &nbsp;
                         <a href={"https://github.com/imalfect"} style={{textDecoration: 'none', color: '#cd3c6d'}}>
                             iMalFect
                         </a>
