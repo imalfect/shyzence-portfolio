acb29c9ff40a55c6bf0459147ec73a5fda942ade
Shyzence <117179990+Shyzence@users.noreply.github.com>
2023-09-07 14:54:09 -0400

    Update AppBar.jsx

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -19,19 +19,19 @@ export default function TopBar() {
                     </Typography>
                     <Button
                         color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 10}}
+                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 16}}
                         href="#">
                         Home
                     </Button>
                     <Button
                         color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 10}}
+                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 16}}
                         href="#work">
                         Work
                     </Button>
                     <Button
                         color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 10}}
+                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 16}}
                         href="#contact">
                         Contact
                     </Button>
