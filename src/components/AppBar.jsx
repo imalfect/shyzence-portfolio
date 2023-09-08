669d923b613c38ab1c787fe04a369d9db28ba10e
iMalFect <me@imal.dev>
2023-09-08 23:20:37 +0200

    Fix button alignment

--- src/components/AppBar.jsx
+++ src/components/AppBar.jsx
@@ -2,41 +2,45 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import Container from "@mui/material/Container";
 import ShyzenceAvatar from '../assets/avatar2.webp'
 import Avatar from "@mui/material/Avatar";
 import ColorModeSwitch from "./ColorModeSwitch.jsx";
 export default function TopBar() {
     return (
         <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static" sx={{backgroundColor: 'background.appbar'}}>
-                <Toolbar>
-                    <Avatar src={ShyzenceAvatar} sx={{mr: 2}} variant="square"/>
-                    {/**Typography style={{ fontSize: '17px' }} component="div" sx={{ flexGrow: 1, fontWeight: 700 }}>
-                        Shyzence
-                    </Typography>**/}
-                    <ColorModeSwitch/>
-                    <Button
-                        color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 17}}
-                        href="#">
-                        Home
-                    </Button>
-                    <Button
-                        color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 17}}
-                        href="#work">
-                        Work
-                    </Button>
-                    <Button
-                        color="inherit"
-                        sx={{fontWeight: 700, textTransform: 'capitalize', fontSize: 17}}
-                        href="#contact">
-                        Contact
-                    </Button>
+            <AppBar position="static" sx={{ backgroundColor: 'background.appbar' }}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between' }}>
+                    <div style={{ display: 'flex', alignItems: 'center' }}>
+                        <Avatar src={ShyzenceAvatar} sx={{ mr: 2 }} variant="square" />
+                        {/* Your Avatar */}
+                        <ColorModeSwitch />
+                        {/* Your ColorModeSwitch component */}
+                    </div>
+                    <div>
+                        <Button
+                            color="inherit"
+                            sx={{ fontWeight: 700, textTransform: 'capitalize', fontSize: 17 }}
+                            href="#"
+                        >
+                            Home
+                        </Button>
+                        <Button
+                            color="inherit"
+                            sx={{ fontWeight: 700, textTransform: 'capitalize', fontSize: 17 }}
+                            href="#work"
+                        >
+                            Work
+                        </Button>
+                        <Button
+                            color="inherit"
+                            sx={{ fontWeight: 700, textTransform: 'capitalize', fontSize: 17 }}
+                            href="#contact"
+                        >
+                            Contact
+                        </Button>
+                    </div>
                 </Toolbar>
             </AppBar>
         </Box>
