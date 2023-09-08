7965b2f5656ec0ceb147e4c249692667c577ea82
Shyzence <117179990+Shyzence@users.noreply.github.com>
2023-09-08 09:12:55 -0400

    Update GetInTouchLabel.jsx

--- src/components/GetInTouchLabel.jsx
+++ src/components/GetInTouchLabel.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Typography from "@mui/material/Typography";
 export default function GetInTouchLabel() {
     return (
-        <Typography variant="h4" component="h4" sx={{textAlign: 'center', mt: 2}} id="contact">
+        <Typography variant="h4" component="h4" sx={{textAlign: 'center', mt: 5}} id="contact">
             Get in touch
         </Typography>
     )
